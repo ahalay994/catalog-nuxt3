1a2c5ce4bc33409f0ad185f2369f83c299d1eec0
Nikolay Gudyno <gudyno@lbr.ru>
2024-12-17 22:40:03 +0100

    Test create API

--- pages/Home.vue
+++ pages/Home.vue
@@ -2,10 +2,16 @@
   <div>
     <h1>Главная</h1>
     <NuxtLink :to="{ name: 'catalog' }">Каталог</NuxtLink>
+
+    <div>
+      <p>API:</p>
+      <pre>{{ data }}</pre>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const {data} = await useFetch('/api/hello')
 
 </script>
 
